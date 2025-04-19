e0fafec9603e953543990e3a788ff7dbe09318c7
Abdullah Usama <140763686+AbdullahUsama087@users.noreply.github.com>
2025-04-19 14:15:02 +0200

    Add Violations, DiscountsController and CustomerDiscounts

---
+++ Admin-Dashboard/Admin-Dashboard/Controllers/CustomerDiscountsController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Admin_Dashboard.Models;
+
+namespace Admin_Dashboard.Controllers
+{
+    public class CustomerDiscountsController : Controller
+    {
+        private readonly SanayiiContext _context;
+
+        public CustomerDiscountsController(SanayiiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CustomerDiscounts
+        public async Task<IActionResult> Index()
+        {
+            var sanayiiContext = _context.CustomerDiscounts.Include(c => c.Customer).Include(c => c.Discount);
+            return View(await sanayiiContext.ToListAsync());
+        }
+
+        // GET: CustomerDiscounts/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerDiscount = await _context.CustomerDiscounts
+                .Include(c => c.Customer)
+                .Include(c => c.Discount)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customerDiscount == null)
+            {
+                return NotFound();
+            }
+
+            return View(customerDiscount);
+        }
+
+        // GET: CustomerDiscounts/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id");
+            ViewData["DiscountId"] = new SelectList(_context.Discounts, "Id", "Name");
+            return View();
+        }
+
+        // POST: CustomerDiscounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CustomerId,DiscountId,DateGiven")] CustomerDiscount customerDiscount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(customerDiscount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", customerDiscount.CustomerId);
+            ViewData["DiscountId"] = new SelectList(_context.Discounts, "Id", "Name", customerDiscount.DiscountId);
+            return View(customerDiscount);
+        }
+
+        // GET: CustomerDiscounts/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerDiscount = await _context.CustomerDiscounts.FindAsync(id);
+            if (customerDiscount == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", customerDiscount.CustomerId);
+            ViewData["DiscountId"] = new SelectList(_context.Discounts, "Id", "Name", customerDiscount.DiscountId);
+            return View(customerDiscount);
+        }
+
+        // POST: CustomerDiscounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("CustomerId,DiscountId,DateGiven")] CustomerDiscount customerDiscount)
+        {
+            if (id != customerDiscount.CustomerId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(customerDiscount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CustomerDiscountExists(customerDiscount.CustomerId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customers, "Id", "Id", customerDiscount.CustomerId);
+            ViewData["DiscountId"] = new SelectList(_context.Discounts, "Id", "Name", customerDiscount.DiscountId);
+            return View(customerDiscount);
+        }
+
+        // GET: CustomerDiscounts/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var customerDiscount = await _context.CustomerDiscounts
+                .Include(c => c.Customer)
+                .Include(c => c.Discount)
+                .FirstOrDefaultAsync(m => m.CustomerId == id);
+            if (customerDiscount == null)
+            {
+                return NotFound();
+            }
+
+            return View(customerDiscount);
+        }
+
+        // POST: CustomerDiscounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var customerDiscount = await _context.CustomerDiscounts.FindAsync(id);
+            if (customerDiscount != null)
+            {
+                _context.CustomerDiscounts.Remove(customerDiscount);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CustomerDiscountExists(string id)
+        {
+            return _context.CustomerDiscounts.Any(e => e.CustomerId == id);
+        }
+    }
+}
---
+++ Admin-Dashboard/Admin-Dashboard/Controllers/ViolationsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Admin_Dashboard.Models;
+
+namespace Admin_Dashboard.Controllers
+{
+    public class ViolationsController : Controller
+    {
+        private readonly SanayiiContext _context;
+
+        public ViolationsController(SanayiiContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Violations
+        public async Task<IActionResult> Index()
+        {
+            var sanayiiContext = _context.Violations.Include(v => v.Contract);
+            return View(await sanayiiContext.ToListAsync());
+        }
+
+        // GET: Violations/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var violation = await _context.Violations
+                .Include(v => v.Contract)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (violation == null)
+            {
+                return NotFound();
+            }
+
+            return View(violation);
+        }
+
+        // GET: Violations/Create
+        public IActionResult Create()
+        {
+            ViewData["ContractId"] = new SelectList(_context.Contracts, "Id", "ArtisanId");
+            return View();
+        }
+
+        // POST: Violations/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Reason,Status,Date,ContractId")] Violation violation)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(violation);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContractId"] = new SelectList(_context.Contracts, "Id", "ArtisanId", violation.ContractId);
+            return View(violation);
+        }
+
+        // GET: Violations/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var violation = await _context.Violations.FindAsync(id);
+            if (violation == null)
+            {
+                return NotFound();
+            }
+            ViewData["ContractId"] = new SelectList(_context.Contracts, "Id", "ArtisanId", violation.ContractId);
+            return View(violation);
+        }
+
+        // POST: Violations/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Reason,Status,Date,ContractId")] Violation violation)
+        {
+            if (id != violation.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(violation);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ViolationExists(violation.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ContractId"] = new SelectList(_context.Contracts, "Id", "ArtisanId", violation.ContractId);
+            return View(violation);
+        }
+
+        // GET: Violations/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var violation = await _context.Violations
+                .Include(v => v.Contract)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (violation == null)
+            {
+                return NotFound();
+            }
+
+            return View(violation);
+        }
+
+        // POST: Violations/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var violation = await _context.Violations.FindAsync(id);
+            if (violation != null)
+            {
+                _context.Violations.Remove(violation);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ViolationExists(int id)
+        {
+            return _context.Violations.Any(e => e.Id == id);
+        }
+    }
+}
